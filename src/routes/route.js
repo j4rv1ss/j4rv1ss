c64535b34e8d5f01852598060644cadd1b655934
j4rv1ss <nagpureanshul7@gmail.com>
2022-06-09 14:49:41 +0530

    modift files

--- src/routes/route.js
+++ src/routes/route.js
@@ -1,7 +1,5 @@
 const express = require('express');
 const router = express.Router();
-// const bookModel= require("../models/bookModel")
-// const UserModel= require("../models/userModel")
 const UserController= require("../controllers/userController")
 const BookController= require("../controllers/bookController")
 
